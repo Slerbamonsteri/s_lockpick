04e609bfc4c7434d5fad2d28c63bec6f18d496ec
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2021-08-13 05:10:30 +0300

    Update sv.lua

--- s_lockpick/sv.lua
+++ s_lockpick/sv.lua
@@ -3,6 +3,6 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 print('started resource')
 
-ESX.RegisterUsableItem('lockpick', function(source)
+ESX.RegisterUsableItem('vehlockpick', function(source)
 	TriggerClientEvent('s_lockpick:startlockpicking', source)
 end)
