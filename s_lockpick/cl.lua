762452dd2dbb6042970a8d91b3b2705625ed6a61
Slerbamonsteri <75551384+Slerbamonsteri@users.noreply.github.com>
2021-08-13 05:10:00 +0300

    Update cl.lua

--- s_lockpick/cl.lua
+++ s_lockpick/cl.lua
@@ -2,7 +2,7 @@ ESX = nil
 
 Citizen.CreateThread(function()
 	while ESX == nil do
-		TriggerEvent('pkrp:getSharedObject', function(obj) ESX = obj end)
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Wait(0)
 	end
 end)
